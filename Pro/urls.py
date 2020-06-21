6c96bb46056e9d41de6a45ebfe25bd2847506e99
AjRock <you@example.com>
2020-06-21 23:18:33 +0530

    ok

--- Pro/urls.py
+++ Pro/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pro_app import views
+from pro_app import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
